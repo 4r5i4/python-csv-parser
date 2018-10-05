4917934bd04bd9eb1b964af52fc1c7d386173a41
Arsia Ardalan <parthiardalan@gmail.com>
2018-10-05 14:42:25 -0400

    checking num of args, need at least two Also using pipenv to lock down deps

--- .vscode/test-env.py
+++ .vscode/test-env.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 
+import sys
+import keyboard
+
+
+def runtime_arg_check(arg_length):
+    ''' takes number of arguments from command line and checks 
+        to see if it is exactly 2'''
+    print('length of args is %d' % (arg_length))
+
+    if arg_length < 2 | arg_length > 2:
+        # raising custom exception
+        raise TypeError(
+            f'expected excatly 2 arguments, got {arg_length} args instead!'
+            f'\n\tTry: >python parser.py <filename>.csv'
+        )
+
 
 def main():
-    print('testing env')
+    # check for correct number of args
+    try:
+        runtime_arg_check(len(sys.argv))
+    except TypeError as e:
+        print(f'Range error: {e}')
+
+    try:
+        while True:
+            if keyboard.is_pressed('Esc'):
+                print("\nyou pressed Esc, so exiting...")
+                sys.exit(0)
+    except BaseException as e:
+        print(f'exiting...')
 
 
 if __name__ == "__main__":
